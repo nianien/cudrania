f3900b843c28eabbc57ae119a72ae636ceccd457
skyfalling <lining15@kuaishou.com>
2022-07-21 00:39:08 +0800

    多bit进制

--- src/test/java/com/cudrania/test/MultiBitTest.java
+++ src/test/java/com/cudrania/test/MultiBitTest.java
@@ -13,10 +13,21 @@ public class MultiBitTest {
 
     @Test
     public void test() {
-        MultiBit mb = MultiBit.hex().set(14, 15).set(13, 14);
-        assert mb.toString().equals("fe00000");
-        assert mb.apply(1) == 0xfe00001;
-        System.out.println(Long.toString(mb.apply(1), 16));
+        MultiBit mb1 = MultiBit.hex().set(0, 4);
+        MultiBit mb2 = MultiBit.hex().set(1, 5);
+        MultiBit mb3 = MultiBit.hex().set(2, 6);
+        int n = 1;
+        long r1 = mb1.apply(mb2.apply(mb3.apply(n)));
+        long r2 = mb3.apply(mb2.apply(mb1.apply(n)));
+        System.out.println(Long.toHexString(r1));
+        System.out.println(Long.toHexString(r2));
+        System.out.println(mb3.apply("n"));
+        System.out.println(mb2.apply("n"));
+        System.out.println(mb1.apply("n"));
+        n=n & -241 | 80;
+        n=n & -3841 | 1536;
+        n=n & -16 | 4;
+        System.out.println(Long.toHexString(n));
     }
 
 }
